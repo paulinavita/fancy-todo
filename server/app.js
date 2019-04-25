4e511e8f43fec11c4e87523c97d5ceed519e67c0
paulinavita <paulinadavita@gmail.com>
2019-04-25 20:00:34 +0700

    deployement, add edit todo

--- server/app.js
+++ server/app.js
@@ -7,12 +7,15 @@ const mongoose = require('mongoose')
 const jwt = require('jsonwebtoken');
 const cors = require('cors')
 const routes = require('./routes')
+const uri = `mongodb+srv://paulina:paulina@fancytodo-qvjsj.mongodb.net/test`
 
-
-mongoose.connect('mongodb://localhost/fancy_todo', { 
+mongoose.connect(uri, { 
   useNewUrlParser: true,
   useCreateIndex: true 
-});
+})
+.then(() => console.log(`=======Mongo CONNECTED========`))
+.catch(err => console.log(err))
+
 
 
 app
